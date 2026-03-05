ddecb4710f7bcf1f58c73216521e80520f02ce31
tomassmusso <tomasmusso28@gmail.com>
2026-03-05 14:12:14 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'music-tracker-v1';
+const assets = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
